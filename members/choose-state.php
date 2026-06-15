880f553f0b58ceac50d2c308e158de72c587f401
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2026-06-15 07:30:31 +0100

    Update choose-state.php

--- members/choose-state.php
+++ members/choose-state.php
@@ -93,7 +93,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
                 $pdo->rollBack();
 
-                die($e->getMessage());
+                $message = '<div class="alert-error">Unable to process your request.</div>';
             }
         }
     }
