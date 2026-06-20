88097f02c55bd69b1fda4e2ffa29d9d1aab48072
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2026-06-20 10:44:53 +0100

    Update withdrawal-history.php

--- members/withdrawal-history.php
+++ members/withdrawal-history.php
@@ -208,7 +208,7 @@ include 'includes/navbar.php';
                         
                             <!-- ACTION -->
                             <td>
-                                <a href="withdrawal-receipt?id=<?= (int)$row['id'] ?>"
+                                <a href="withdraw-receipt?id=<?= (int)$row['id'] ?>"
                                    class="receipt-btn">
                                     View Receipt
                                 </a>
