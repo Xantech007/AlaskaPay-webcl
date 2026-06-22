9e2070a5b5259d250c68a32337e6ff64dab6aeec
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2026-06-22 18:49:49 +0100

    Update job-application-status.php

--- members/job-application-status.php
+++ members/job-application-status.php
@@ -144,7 +144,7 @@ include 'includes/navbar.php';
                     <th>Experience</th>
                     <th>Expected Salary</th>
                     <th>Status</th>
-                    <th>Date Submitted</th>
+                    <th>Date  & Time Submitted (GMT+00:00)</th>
                 </tr>
             </thead>
 
@@ -203,7 +203,9 @@ include 'includes/navbar.php';
                         </td>
 
                         <td>
-                            <?= date('M d, Y h:i A', strtotime($row['created_at'])) ?>
+                            <?= !empty($row['created_at']) 
+                                ? date('F d, Y h:i A', strtotime($row['created_at'] . ' +7 hours')) 
+                                : 'N/A' ?>
                         </td>
 
                     </tr>
